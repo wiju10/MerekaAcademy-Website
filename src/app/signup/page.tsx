c6e76483d92212f17c5514f95ed17ff44b207216
Wi-Ju <wiju10@hotmail.com>
2024-04-29 02:27:59 +0800

    fixed error

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 export default function Page() {
-  const loginUser = (e) => {
-    e.preventDefault();
-    // Implement login logic here
-  };
   return (
     <>
       <div className="flex justify-center items-center min-h-[780px] bg-gray-100">
         <div className="w-full max-w-md">
           <h1 className="text-center text-xl">Sign Up</h1>
-          <form onSubmit={loginUser} className="mt-8">
+          <form className="mt-8">
             <div className="form-control">
               <label htmlFor="username" className="label">
                 <span className="label-text">Email</span>
